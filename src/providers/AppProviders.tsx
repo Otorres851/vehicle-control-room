41924409e92bf196e8d5c2f5e039f93a046affec
Oscar Torres <oscar4545@hotmail.com>
2026-06-02 23:41:36 -0500

    feature(vehicle-control-test): We have prepared the professional data layer

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,5 +1,10 @@
+import { QueryProvider } from "./QueryProvider";
 import { ThemeProvider } from "./ThemeProvider";
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <QueryProvider>{children}</QueryProvider>
+    </ThemeProvider>
+  );
 }
